d679bfb396acccb35c5b8229f38e4bc8e768b5f0
piotr <piotr.dominik.sobol@gmail.com>
2023-03-30 01:02:39 +0200

    Add 3 addional columns to dataset by web scrapping

--- scratchpad.py
+++ scratchpad.py
@@ -18,10 +18,17 @@ for page_number in range(1, 563):
         
         price = article.find_all('span', attrs={'class': 'ooa-1bmnxg7 eayvfn611'})
         rest = article.find_all('li', attrs={'class': 'ooa-1k7nwcr e19ivbs0'})
-        if(rest[0].text == 'Niski przebieg'): row = VolkswagenModel(price[0].text, rest[1].text, rest[2].text, rest[3].text)
-        else : row = VolkswagenModel(price[0].text, rest[0].text, rest[1].text, rest[2].text)
+        model = article.find_all('a')
+        estimations = article.find_all('p', attrs={'class': 'e1xj1nw30 ooa-ixk3xj er34gjf0'})
+
+        if rest[0].text == 'Niski przebieg' and len(estimations) == 0: row = VolkswagenModel(price[0].text, rest[1].text, rest[2].text, rest[3].text, rest[4].text, model[0].text, '-')
+        elif rest[0].text != 'Niski przebieg' and len(estimations) == 0: row = VolkswagenModel(price[0].text, rest[0].text, rest[1].text, rest[2].text, rest[3].text, model[0].text, '-')
+        elif rest[0].text == 'Niski przebieg' and len(estimations) != 0: row = VolkswagenModel(price[0].text, rest[1].text, rest[2].text, rest[3].text, rest[4].text, model[0].text, estimations[0].text)
+        elif rest[0].text != 'Niski przebieg' and len(estimations) != 0: row = VolkswagenModel(price[0].text, rest[0].text, rest[1].text, rest[2].text, rest[3].text, model[0].text, estimations[0].text)
+        
         row.clean_data()
         data.append(row.return_data())  
 
 df = pd.DataFrame(data)
+print(df)
 df.to_csv('./AI_UniClasses_Assesment/data/otomoto.csv')
